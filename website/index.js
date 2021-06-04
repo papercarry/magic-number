1d60e60a6cc0ffd71a2a6213abef090f9ca18326
Jeremiah Ang <s43500@sp.edu.sg>
2021-06-04 17:01:36 +0800

    Complete Step 1 [Level 1]

---
+++ website/index.js
@@ -0,0 +1,12 @@
+// Listening for the event when the DOM is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    alert('DOM Loaded!');
+    // Getting a reference of the button using document.getElementById(...)
+    const createSessionButton = document.getElementById('create-session-button');
+
+    // Adding an event listener on createSessionButton for the "Click" event
+    createSessionButton.addEventListener('click', function () {
+        // This callback function is executed when the event is fired
+        alert('Button pressed!');
+    });
+});
