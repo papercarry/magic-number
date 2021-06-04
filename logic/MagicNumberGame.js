4a7f1279d5925f502f73ed3ef4c335d45477c246
Jeremiah Ang <s43500@sp.edu.sg>
2021-06-04 17:22:51 +0800

    Complete Constructor [Level 1.5]

--- logic/MagicNumberGame.js
+++ logic/MagicNumberGame.js
@@ -4,4 +4,14 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-module.exports = class {};
+module.exports = class {
+    // Constructor method takes in max as parameter.
+    constructor(max) {
+        // Generate magicNumber from 1 to max - 1 (both inclusive)
+        this.magicNumber = getRandomInt(1, max);
+        // Set lower and upper bound.
+        this.lowerBound = 0;
+        this.upperBound = max;
+    }
+
+};
