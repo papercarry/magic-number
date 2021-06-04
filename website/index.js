4cf080a7421fc9cf32da9be53213b93d1c4e0e8a
Jeremiah Ang <s43500@sp.edu.sg>
2021-06-04 17:13:52 +0800

    Complete Step 5 [Level 1]

--- website/index.js
+++ website/index.js
@@ -8,6 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
     createSessionButton.addEventListener('click', function () {
         // This callback function is executed when the event is fired
         alert('Button pressed!');
-        fetch('http://localhost:8000', { method: 'POST' });
+        fetch('http://localhost:8000', { method: 'POST' })
+            // first .then, receive the response
+            .then(function (response) {
+                // Treat the body as json and convert it into JavaScript Object
+                return response.json();
+            })
+            // second .then, where we can access the JavaScript Object
+            .then(function (json) {
+                // extract the session Id from response body
+                const sessionId = json.session_id;
+
+                // Get reference to the element that will display the session id
+                const sessionIdField = document.getElementById('session-id-field');
+
+                // Set the innerHTML to the session id
+                sessionIdField.innerHTML = sessionId;
+            });
     });
 });
